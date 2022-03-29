a05b5c07a289bdb7e91e2300bb4f369fa86d13a8
Robson Thomaz Castelli <robsoncastelli@gmail.com>
2022-03-28 21:32:43 -0300

    Aula 3

---
+++ classes/curso.php
@@ -0,0 +1,13 @@
+<?php
+class Curso
+{
+  // propriedades da classe
+  public $nome = "";
+  public $tipo  = "";
+  public $ativo = true;
+
+  function __construct()
+  {
+    //
+  }
+}
--- index.php
+++ index.php
@@ -1,1 +1,78 @@
 <?php
+// importação das classes
+require_once('classes/curso.php');
+
+$cursos = array();
+
+// adicionar c#
+$curso = new Curso();
+$curso->nome = "C#";
+$curso->tipo = "Programação";
+$curso->ativo = false;
+array_push($cursos, $curso);
+
+// adicionar MySQL
+$curso = new Curso();
+$curso->nome = "MySQL";
+$curso->tipo = "Banco de dados";
+array_push($cursos, $curso);
+
+// adicionar Node JS
+$curso = new Curso();
+$curso->nome = "Node JS";
+$curso->tipo = "Javascript";
+array_push($cursos, $curso);
+
+// adicionar React
+$curso = new Curso();
+$curso->nome = "React JS";
+$curso->tipo = "Javascript";
+array_push($cursos, $curso);
+?>
+
+<html>
+
+<head>
+  <title>Aula 3</title>
+
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    header {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      width: 100%;
+      min-height: 60px;
+      background-color: #9f9f9f;
+      padding-left: 20px;
+      margin-bottom: 20px;
+    }
+
+    div {
+      padding: 20px;
+    }
+  </style>
+</head>
+
+<body>
+  <?php include('_header.php'); ?>
+
+  <div>
+    <ul>
+      <?php
+      foreach ($cursos as $curso) {
+        if ($curso->ativo == true) {
+          echo "<li>" .  $curso->nome . " - <i>" . $curso->tipo . "</i></li>";
+        }
+      }
+      ?>
+    </ul>
+  </div>
+</body>
+
+</html>
